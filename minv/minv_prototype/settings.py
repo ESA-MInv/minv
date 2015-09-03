1d5ff8052e5559db6320b89770255a9ff9b3be13
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2015-09-03 10:25:55 +0200

    Fixing messages for Django 1.6.

--- minv/minv_prototype/settings.py
+++ minv/minv_prototype/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE_CLASSES = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
 )
 
 ROOT_URLCONF = 'minv_prototype.urls'
