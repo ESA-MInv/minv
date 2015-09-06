9c6900ad8ecb00980b169729d3808e79f5fdc2a5
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2015-09-06 11:13:24 +0200

    Improced backup/restore views.

--- minv/minv/inventory/views.py
+++ minv/minv/inventory/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from minv.inventory import models
 from minv.inventory import forms
@@ -49,20 +50,40 @@ def root_view(request):
 
 @login_required(login_url="login")
 def backup_view(request):
+    if request.method == "POST":
+        form = forms.BackupForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            messages.info(
+                request, "Started %s backup of %s." % (
+                    data["backup_type"], ", ".join(data["subject"])
+                )
+            )
+    else:
+        form = forms.BackupForm()
     return render(
         request, "inventory/backup.html", {
             "collections": models.Collection.objects.all(),
-            "form": forms.BackupForm()
+            "form": form
         }
     )
 
 
 @login_required(login_url="login")
 def restore_view(request):
+    if request.method == "POST":
+        form = forms.RestoreForm(request.POST)
+        if form.is_valid():
+            messages.info(
+                request,
+                "Restored instance from '%s'." % form.cleaned_data["backup"]
+            )
+    else:
+        form = forms.RestoreForm()
     return render(
         request, "inventory/restore.html", {
             "collections": models.Collection.objects.all(),
-            "form": forms.RestoreForm()
+            "form": form
         }
     )
 
