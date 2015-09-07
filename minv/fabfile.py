025e9db47d052d701c142e7327face3431af2ef5
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2015-09-07 16:35:56 +0200

    Adding collectstatic to deploy.

--- minv/fabfile.py
+++ minv/fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import local, env, put, sudo
+from fabric.api import local, env, put, sudo, cd
 
 
 env.hosts = ["5.9.173.45"]
@@ -8,5 +8,7 @@ env.user = "centos"
 def deploy():
     local("python setup.py sdist")
     put("dist/minv-0.0.1.tar.gz", "")
-    sudo("pip install minv-0.0.1.tar.gz  -U")
+    sudo("pip install minv-0.0.1.tar.gz -U")
+    with cd("/var/www/minv_prototype/"):
+        sudo("python manage.py collectstatic --noinput -c")
     sudo("service httpd restart")
