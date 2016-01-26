61cada498c06e195f8214b15143ee9bbabf55332
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-01-26 11:57:45 +0100

    Improving setup of instance. Creating a dummy admin user.

--- minv/vagrant/scripts/development_installation.sh
+++ minv/vagrant/scripts/development_installation.sh
@@ -2,3 +2,14 @@ cd /var/minv
 
 pip install --editable .
 
+# Prepare DB
+python /srv/minv/manage.py syncdb --noinput --traceback
+
+# Create admin user
+python /srv/minv/manage.py shell 1>/dev/null 2>&1 <<EOF
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+if authenticate(username='admin', password='admin') is None:
+    User.objects.create_user('admin', 'admin@admin.ad', 'admin')
+EOF
