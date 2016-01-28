d5a556b5b299167cb9a7ff7ff3e902b9db6f74c3
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-01-28 10:36:59 +0100

    Ensuring httpd starts.

--- minv/vagrant/scripts/packages.sh
+++ minv/vagrant/scripts/packages.sh
@@ -1,5 +1,8 @@
 yum install -y postgresql python-psycopg2 python-pip
 yum install -y httpd mod_wsgi
 
+chkconfig httpd on
+service httpd start
+
 # Python dependencies
 pip install "django<1.7" django_extensions
