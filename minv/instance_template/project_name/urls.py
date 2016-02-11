64a30f45a3b7eb2c0520539251839154d05ce700
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-02-11 13:19:39 +0100

    Added instance template

---
+++ minv/instance_template/project_name/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from minv.inventory.urls import urlpatterns as inventory_urlpatterns
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(inventory_urlpatterns)),
+    url(r'^', include('django.contrib.auth.urls')),
+)
