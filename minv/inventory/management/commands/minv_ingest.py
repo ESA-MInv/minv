5f142d63c2d2eb358bfbc46d958680f36cba8cb4
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-02-15 17:19:05 +0100

    Cleanup and error handling

--- minv/inventory/management/commands/minv_ingest.py
+++ minv/inventory/management/commands/minv_ingest.py
@@ -9,8 +9,7 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option("-m", "--mission", dest="mission"),
         make_option("-f", "--file-type", dest="file_type"),
-        make_option("-u", "--url", dest="url"),
-        #make_option("-i", "--index-file-name", dest="index_file_name"),
+        make_option("-u", "--url", dest="url")
     )
 
     args = '-m MISSION -f FILE TYPE -u URL <index-file-name> ' \
@@ -21,7 +20,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         for index_file_name in args:
-            ingest(
-                options["mission"], options["file_type"], options["url"],
-                index_file_name
-            )
+            try:
+                ingest(
+                    options["mission"], options["file_type"], options["url"],
+                    index_file_name
+                )
+            except Exception as exc:
+                raise CommandError(
+                    "Failed to ingest index file '%s'. Error was: %s"
+                    % (index_file_name, exc)
+                )
