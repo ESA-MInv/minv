c1846673b355c5603fa74e20871f9d20eafb491d
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-02-16 14:19:58 +0100

    Improving fabric deployment

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,41 @@
-from fabric.api import local, env, put, sudo, cd
+from os.path import dirname, join
+
+from fabric.api import local, put, sudo, lcd, env
+
+import minv
 
 
-env.hosts = ["5.9.173.45"]
-env.user = "centos"
-
-
-def deploy():
-    local("python setup.py sdist")
-    put("dist/minv-0.0.1.tar.gz", "")
-    sudo("pip install minv-0.0.1.tar.gz -U")
-    with cd("/var/www/minv_prototype/"):
-        sudo("python manage.py collectstatic --noinput -c")
+def deploy(uninstall=False, version=None):
+    version = version or minv.__version__
+    put(join(env.builder_path, "build/RPMS/minv-%s-1.noarch.rpm" % version), "")
+    if uninstall:
+        sudo("yum remove -y minv")
+    sudo("yum install -y minv-%s-1.noarch.rpm" % version)
     sudo("service httpd restart")
+
+
+def archive(version=None):
+    version = version or minv.__version__
+    with lcd(dirname(__file__)):
+        local(
+            "git archive --format=tar --prefix=MInv-{version}/ master "
+            "| gzip > MInv-{version}.tar.gz"
+            "; mv MInv-{version}.tar.gz ../../builder/build/SOURCES/".format(
+                version=version
+            )
+        )
+
+
+def build(version=None):
+    version = version or minv.__version__
+
+    with lcd(env.builder_path):
+        local("vagrant up")
+        local("vagrant ssh -c '%s'" % " ; ".join([
+            "rm -rf MInv-{version}",
+            "cp rpmbuild/SOURCES/MInv-{version}.tar.gz .",
+            "tar -xzf MInv-{version}.tar.gz",
+            "cd MInv-{version}",
+            "python setup.py bdist_rpm",
+            "cp dist/minv-{version}*rpm ../rpmbuild/RPMS/"
+        ]).format(version=version))
