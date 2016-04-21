89c904e5991c4e7a97d53c915eee2abf2a724796
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-04-21 16:29:04 +0200

    Allowing to pass more than one collection URL when harvesting

--- minv/inventory/management/commands/harvest.py
+++ minv/inventory/management/commands/harvest.py
@@ -9,15 +9,24 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option("-m", "--mission", dest="mission"),
         make_option("-f", "--file-type", dest="file_type"),
-        make_option("-u", "--url", dest="url")
+        make_option("-u", "--url", dest="urls",
+            action="append", help="The harvesting location to harvest.")
     )
 
-    args = '-m MISSION -f FILE TYPE -u URL <location-url> ' \
-           '[<index-file-name> ...]'
+    args = '-m MISSION -f FILE TYPE -u <location-url> [ -u <location-url> ... ]'
 
     help = 'Harvest the whole collection or specific locations of a collection.'
 
     def handle(self, *args, **options):
-        harvest(
-            options["mission"], options["file_type"], options["url"]
-        )
+        if not options["urls"]:
+            raise CommandError("No location URLs specified.")
+
+        for url in options["urls"]:
+            try:
+                harvest(
+                    options["mission"], options["file_type"], url
+                )
+            except Exception as exc:
+                raise CommandError(
+                    "Failed to harvest location %s. Error was: %s" % (url, exc)
+                )
