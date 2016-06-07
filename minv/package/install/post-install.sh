c5f00d7987783a0267ce85adc588f4acc6a417f9
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-06-07 13:31:31 +0200

    Fixing permissions / ownership of install files

--- minv/package/install/post-install.sh
+++ minv/package/install/post-install.sh
@@ -4,19 +4,23 @@ LOG_DIR=/var/log/minv
 INSTALL_DIR=$(dirname `python -c "import minv; print(minv.__file__)"`)
 
 mkdir -p $CONF_DIR/collections
-chown -R minv:minv $CONF_DIR/collections
+chmod 0770 $CONF_DIR/collections
 chown minv:minv $CONF_DIR/minv.conf
+chown -R minv:minv $CONF_DIR/collections
+
 mkdir -p $LOG_DIR
 mkdir -p $LOG_DIR/collections
 touch $LOG_DIR/minv.log
-chmod 0750 $LOG_DIR
+chmod g+w $LOG_DIR/minv.log
+chmod 0770 $LOG_DIR
 chown -R minv:minv $LOG_DIR
+
 mkdir -p /var/lock/minv/
 chown -R minv:minv /var/lock/minv/
 mkdir -p $DATA_DIR/collections
 chown minv:minv $DATA_DIR/collections
-chmod 0755 $DATA_DIR/collections
+chmod 0770 $DATA_DIR/collections
 
 django-admin.py startproject minv_instance $DATA_DIR --template $INSTALL_DIR/instance_template
 chown minv:minv -R $DATA_DIR/minv_instance
-chmod 0755 $DATA_DIR/minv_instance/
+chmod 0770 $DATA_DIR/minv_instance/
