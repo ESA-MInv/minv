6fd812ba2598c0504ae5d49fec176e5956159877
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-06-27 12:06:03 +0200

    Adding Logging to monitoring

--- minv/tasks/api.py
+++ minv/tasks/api.py
@@ -1,10 +1,23 @@
 from json import dumps
 import traceback
 import json
+from datetime import datetime
+import logging
 
 from django.utils.timezone import now
 
 from minv.tasks import models
+
+
+logger = logging.getLogger(__name__)
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError("Type not serializable")
 
 
 def monitor(task_or_job, **kwargs):
@@ -12,7 +25,9 @@ def monitor(task_or_job, **kwargs):
     """
     if isinstance(task_or_job, basestring):
         job = models.Job.objects.create(
-            task=task_or_job, arguments=dumps(kwargs, indent=2)
+            task=task_or_job, arguments=dumps(
+                kwargs, indent=2, default=json_serial
+            )
         )
     else:
         job = task_or_job
@@ -31,19 +46,29 @@ class JobContext(object):
         self.job.start_time = now()
         self.job.full_clean()
         self.job.save()
+        logger.info("Starting job %s of task %s." % (self.job, self.job.task))
         return self
 
     def __exit__(self, etype=None, value=None, tb=None):
+        self.job.end_time = now()
+        self.job.full_clean()
+        self.job.save()
+
         if (etype, value, tb) == (None, None, None):
             self.job.status = "finished"
+            logger.info(
+                "Job %s of task %s finished after %s." % (
+                    self.job, self.job.task, self.job.run_time
+                )
+            )
         else:
             self.job.status = "failed"
             self.job.error = str(value)
             self.job.traceback = "\n".join(traceback.format_tb(tb))
-
-        self.job.end_time = now()
-        self.job.full_clean()
-        self.job.save()
+            logger.error("Job %s of task %s failed after %s." % (
+                    self.job, self.job.task, self.job.run_time
+                )
+            )
 
 
 def schedule(task, when, arguments):
